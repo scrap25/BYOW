ed2a4597c84d428f266369c84619d8d17f4ddc30
Maxim Bravo <maximbravo@berkeley.edu>
2022-11-12 16:37:01 -0800

    Modify path generation algorithm slightly and add basic random room generation.

--- byow/TileEngine/World.java
+++ byow/TileEngine/World.java
@@ -1,5 +1,6 @@
 package byow.TileEngine;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class World {
@@ -7,8 +8,8 @@ public class World {
     private final Random random;
     private WorldTile[][] world;
     private int width, height;
-    private Path path;
-
+    private ArrayList<Path> paths;
+    private ArrayList<Room> rooms;
     private TERenderer ter;
 
     public World(long seed, int width, int height, TERenderer ter) {
@@ -17,7 +18,38 @@ public class World {
         this.width = width;
         this.height = height;
         world = generateEmptyWorld(width, height);
-        path = generateMainPath();
+        paths = generatePaths(0);
+//        rooms = generateRooms(15);
+    }
+
+    private ArrayList<Path> generatePaths(int delay) {
+        ArrayList<Path> newPaths = new ArrayList<>();
+        int xfourth = Math.round(width/4);
+        int yfourth = Math.round(height/4);
+        int[][] startingPoints = {{xfourth, yfourth},{xfourth, 3*yfourth},{3*xfourth, yfourth},{3*xfourth, 3*yfourth}};
+        for (int i = 0; i < startingPoints.length; i++) {
+            int[] xy = startingPoints[i];
+            newPaths.add(generatePath(xy[0], xy[1], delay));
+        }
+        return newPaths;
+    }
+
+    private ArrayList<Room> generateRooms(int numRooms)  {
+        ArrayList<Room> newRooms = new ArrayList<>();
+        for (int i = 0; i < numRooms; i++) {
+            int randRoomWidth = random.nextInt(3, 6);
+            int randRoomHeight = random.nextInt(3, 6);
+
+            int randRoomX = random.nextInt(0, width - randRoomWidth);
+            int randRoomY = random.nextInt(0, height - randRoomHeight);
+
+            for (int x = randRoomX; x < randRoomX + randRoomWidth; x++) {
+                for (int y = randRoomY; y < randRoomY + randRoomHeight; y++) {
+                    world[x][y].makeRoom();
+                }
+            }
+        }
+        return newRooms;
     }
 
     private WorldTile[][] generateEmptyWorld(int width, int height) {
@@ -40,12 +72,11 @@ public class World {
         return teTileWorld;
     }
 
-    private Path generateMainPath() {
-        int steps = 30;
-        int delay = 500; // milliseconds
+    private Path generatePath(int startX, int startY, int delay) {
+        int steps = 20;
         Path newPath = new Path();
-        int x = random.nextInt(width);
-        int y = random.nextInt(height);
+        int x = startX;//Math.round(width / 2);//random.nextInt(width);
+        int y = startY; //Math.round(height / 2);//random.nextInt(height);
         System.out.println("Starting at (" + x + ", " + y + ")");
         world[x][y].placeAvatar();
         newPath.add(world[x][y]);
@@ -53,7 +84,9 @@ public class World {
         int lastDirectionIndex = -1;
         while (stepsTaken < steps) {
             int[] xyDirectionAndLength = getRandomDirectionAndLength(x, y, lastDirectionIndex % 2, 0.75);
-            lastDirectionIndex = xyDirectionAndLength[0];
+            if (xyDirectionAndLength == null) {
+                continue;
+            }
             System.out.println(stepsTaken + ": Going direction " + lastDirectionIndex + " for " + Math.max(xyDirectionAndLength[3], xyDirectionAndLength[4]));
             for (int i = 0; i < xyDirectionAndLength[3]; i++) {
                 x += xyDirectionAndLength[1];
@@ -66,10 +99,13 @@ public class World {
                 newPath.add(world[x][y]);
             }
             ter.renderFrame(getAsTETiles());
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            lastDirectionIndex = xyDirectionAndLength[0];
+            if (delay > 0) {
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
             stepsTaken++;
         }
@@ -77,30 +113,45 @@ public class World {
         return newPath;
     }
 
+    /**
+     *
+     * @param x X position of path head
+     * @param y Y position of path head
+     * @param forbiddenDirection Either 0 (right, left) or 1 (down, up)
+     * @param factor Factor to multiply final step length.
+     * @return
+     */
     private int[] getRandomDirectionAndLength(int x, int y, int forbiddenDirection, double factor) {
+        int constantLength = 10;
         //                   0:right   1:down   2:left    3:up
         int[][] directions = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+        // {-1, 0} represents 1 unit in -x direction and 0 units in y
         int index = forbiddenDirection;
         while (index % 2 == forbiddenDirection) {
+            // Choose a random direction until not one of the forbidden directions
             index = random.nextInt(4);
         }
+
         int[] direction = directions[index];
 
-        int xStepLength = 0;
+        int xLengthTillWall = 0;
         if (direction[0] == -1) {
-            xStepLength = random.nextInt(x + 1);
+            xLengthTillWall = x;
         } else if (direction[0] == 1) {
-            xStepLength = random.nextInt(width - x);
+            xLengthTillWall = width - x - 1;
         }
-        xStepLength = (int) Math.round(xStepLength * factor);
+        int xStepLength = random.nextInt(Math.min(4, xLengthTillWall), Math.min(xLengthTillWall, constantLength) + 1);//(int) Math.round(xStepLength * factor);
 
-        int yStepLength = 0;
+        int yLengthTillWall = 0;
         if (direction[1] == -1) {
-            yStepLength = random.nextInt(y + 1);
+            yLengthTillWall = y;
         } else if (direction[1] == 1) {
-            yStepLength = random.nextInt(height - y);
+            yLengthTillWall = height - y - 1;
         }
-        yStepLength = (int) Math.round(yStepLength * factor);
+        int yStepLength = random.nextInt(Math.min(4, yLengthTillWall), Math.min(yLengthTillWall, constantLength) + 1);//(int) Math.round(yStepLength * factor);
+        if (xStepLength + yStepLength < 5) {
+            return null;
+        }
         return new int[]{index, direction[0], direction[1], xStepLength, yStepLength};
     }
 }
--- byow/TileEngine/WorldGenerator.java
+++ byow/TileEngine/WorldGenerator.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class WorldGenerator {
     private static final long DEFAULT_SEED = 2873123;
-    private static final int WIDTH = 50;
-    private static final int HEIGHT = 50;
+    private static final int WIDTH = 44;
+    private static final int HEIGHT = 44;
 
     public static void main(String[] args) {
         TERenderer ter = new TERenderer();
